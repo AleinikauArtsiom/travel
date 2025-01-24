0e851f1bc4f85538240a3138c861d92b305e2d8f
AleinikauArtsiom <146992392+AleinikauArtsiom@users.noreply.github.com>
2025-01-24 14:38:30 +0300

    1 commit, add dependencies, package structure, create 2 branch - dev

---
+++ src/main/java/com/example/travelpet/TravelPetApplication.java
@@ -0,0 +1,13 @@
+package com.example.travelpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelPetApplication.class, args);
+    }
+
+}
